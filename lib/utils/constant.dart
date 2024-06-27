62df3fdc2d2fc741b1a2668c7e4653b4abcb95c2
Dev Karan Yadav <99685724+TheDevKaran@users.noreply.github.com>
2024-06-27 23:35:52 +0530

    Update constant.dart

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -1,7 +1,7 @@
-String infura_url = "https://sepolia.infura.io/v3/0e084040aebb4084a0c96b5dd85b9781";
+String infura_url = "";
 
-String owner_key = "3278f0dafd9e16450a31fefe9a9f9b49d845e81675d43a4461e8e0ac08ae5115";
+String owner_key = "";
 
-String voter_key = "c2ebf753f7cd89727d7e1af9a476ed88eca2581301097d73efe1804b35e6ce5f";
+String voter_key = "";
 
-String contractAddress1 = "0x54778474BB074c57f1A80c39b5eD7e4b00779Ebf";
+String contractAddress1 = "";
